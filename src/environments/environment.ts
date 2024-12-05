b12f80015622c04b7cea82369c5f26ff34556224
Abdulaziz Assainar <abdulazizassainar.work@gmail.com>
2024-12-05 17:16:58 +0530

    initialCommit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    cloudinary: {
+      cloudName: 'dmyhr9fcz', 
+      apiKey: '828861848193371',  
+      apiSecret: '-xEF_zhb2cZJFwhEMvtl57WAzaU'  
+    },
+    apiUrl: 'http://localhost:3000' 
+  };
+  
