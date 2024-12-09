e3d52fa987cc0ab2baadcce65878dbc39f7f2045
Abdulaziz Assainar <abdulazizassainar.work@gmail.com>
2024-12-09 13:16:29 +0530

    update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import { ProjectsDashboardComponent } from "../shared/projects-dashboard/projects-dashboard.component";
 import { Project } from '../shared/projects-dashboard/project.model';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card'; 
-import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserModule } from '@angular/platform-browser';
+import { TabViewModule } from 'primeng/tabview';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     ProjectsDashboardComponent,
     CommonModule,
-    MatTabsModule,
-    MatCardModule,
+    BrowserModule,
+    TabViewModule,
 ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
